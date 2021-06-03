dc46d0b65a5ac94fc3dacde71c74b42390d0f7fa
Martin Tsvetkov <navaron2@gmail.com>
2021-06-03 19:36:12 +0200

    more optimization fixes

--- SEP6/Controllers/SearchController.cs
+++ SEP6/Controllers/SearchController.cs
@@ -75,29 +75,31 @@ namespace SEP6.Controllers
         [NonAction]
         public List<string> SearchMovie(string searchText)
         {
-            List<string> movies = _moviesContext.Movies.Select(movie => movie.Title).AsParallel().ToList();
+            List<int> movies = _moviesContext.Movies.Select(movie => movie.Id).AsParallel().ToList();
             var ratios = new Dictionary<string,int>();
-            foreach (var movieTitle in movies)
+            string movieTitle;
+            foreach (var movieId in movies)
             {
+                movieTitle = _moviesContext.Movies.FirstOrDefault(a=> a.Id == movieId).Title;
                 var ratio = (int)(Fuzz.Ratio(searchText, movieTitle) * 0.5
                                   + Fuzz.PartialRatio(searchText,movieTitle) * 0.75
                                   + Fuzz.TokenSortRatio(searchText, movieTitle) + 0.75 )/2;
               
                 if (ratios.Count < 10)
                 {
-                    ratios.Add(movieTitle + " " + _moviesContext.Movies.Where(a => a.Title == movieTitle).Select(movies => movies.Year).FirstOrDefault() + ", " + _moviesContext.Movies.Where(a => a.Title == movieTitle).Select(movies => movies.Id).FirstOrDefault(),ratio);
+                    ratios.Add(movieTitle + " " + _moviesContext.Movies.FirstOrDefault(a => a.Id == movieId).Year + ", " + movieId,ratio);
                 }
                 else
                 {
                     var lowestRatio = ratios.Aggregate((l, r) => l.Value < r.Value ? l : r);
                     if (ratio > lowestRatio.Value)
                     {
-                        if (movieTitle + " " +  _moviesContext.Movies.Where(a => a.Title == movieTitle).Select(movies => movies.Year).FirstOrDefault() + ", " + _moviesContext.Movies.Where(a => a.Title == movieTitle).Select(movies => movies.Id).FirstOrDefault() == lowestRatio.Key)
+                        if (movieTitle + " " +  _moviesContext.Movies.FirstOrDefault(a => a.Id == movieId).Year + ", " + movieId == lowestRatio.Key)
                         {
                             throw new Exception("Movies with same title");
                         }
                         ratios.Remove(lowestRatio.Key);
-                        ratios.Add(movieTitle + " " +  _moviesContext.Movies.Where(a => a.Title == movieTitle).Select(movies => movies.Year).FirstOrDefault() + ", " + _moviesContext.Movies.Where(a => a.Title == movieTitle).Select(movies => movies.Id).FirstOrDefault(),ratio);
+                        ratios.Add(movieTitle + " " +  _moviesContext.Movies.FirstOrDefault(a => a.Id == movieId).Year + ", " + movieId,ratio);
                     }
                 }
             }
